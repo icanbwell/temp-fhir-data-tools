9954cb57650ee1ca78f6ffcab74664b1540f4894
Tom Schneider <tom.schneider@icanbwell.com>
2025-07-11 11:37:54 -0400

    PHR-1035 - wip on improvements

---
+++ src/constants/index.js
@@ -0,0 +1,23 @@
+const config = {
+  env: process.env.FHIR_ENVIRONMENT || "dev",
+  useProaServer: process.env.USE_PROA_SERVER === "false",
+};
+
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN || "";
+
+const FHIR_BASE_URL = `https://fhir.${config.env}.icanbwell.com/4_0_0`;
+const PROA_FHIR_BASE_URL = `https://fhir-proa.${config.env}.icanbwell.com/4_0_0`;
+const INTERNAL_FHIR_BASE_URL = `https://fhir-internal.${config.env}.bwell.zone/4_0_0`;
+
+const CLIENT_PATIENT_ID = process.env.CLIENT_PATIENT_ID || "";
+const CLIENT_PERSON_ID = process.env.CLIENT_PERSON_ID || "";
+
+module.exports = {
+  config,
+  ACCESS_TOKEN,
+  FHIR_BASE_URL,
+  PROA_FHIR_BASE_URL,
+  INTERNAL_FHIR_BASE_URL,
+  CLIENT_PATIENT_ID,
+  CLIENT_PERSON_ID,
+};
